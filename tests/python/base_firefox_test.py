c55a0499c15363ece1a184aa6fe48782e77c1d80
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-02-03 14:15:45 +0100

    fix name

--- tests/python/base_firefox_test.py
+++ tests/python/base_firefox_test.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
-class BaseTest(unittest.TestCase):
+class BaseFirefoxTest(unittest.TestCase):
   def setUp(self):
     geckodriver_path = "/snap/bin/geckodriver"
     options = Options()
--- tests/python/javacream.org.test.py
+++ tests/python/javacream.org.test.py
@@ -1,5 +1,5 @@
 import unittest
-from tests.python.base_firefox_test import BaseTest
+from tests.python.base_firefox_test import BaseFirefoxTest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
-class TestDemo(BaseTest):
+class TestDemo(BaseFirefoxTest):
   
   def test_title_is_Javacream(self):
     self.driver.get("http://javacream.org/")
